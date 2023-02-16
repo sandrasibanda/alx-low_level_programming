5aa9b2c91ea19d15f945d7b7434800aab89677be
sandrasibanda <sandra.sibanda992@gmail.com>
2023-02-16 13:05:29 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - prints "and that piece of art is useful" - - Dora Korpar
+ * main - Prints "and that piece of art is useful" - - Dora Korpar
  * followed by a new line, to standard error.
  * Return: 1 always
  */
 int main(void)
 {
-	fwrite(2,
+	write(2,
 		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
