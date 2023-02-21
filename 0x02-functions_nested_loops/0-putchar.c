444347528808427eabfa346ff7f02233a99e795f
sandrasibanda <sandra.sibanda992@gmail.com>
2023-02-21 03:18:03 -0800

    update 0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,10 @@
  * Description: write a program that prints "_putchar" followed by a new line.
  * Return: Always 0 (Success.)
  */
-
 int main(void)
 {
         printf("_putchar");
-	printf("\n");
+	printf("\ni");
 
 	return (0);
 }
