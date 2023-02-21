af53ef66b210e1d07d83200ffd2c764b72f79165
sandrasibanda <sandra.sibanda992@gmail.com>
2023-02-21 13:34:20 -0800

    alpa

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
- * main- The function prints the alphabet in lowercase followed by a new line
  * Return:Always 0 (Success)
  * print_alphabet - prints the alphabet in lowercase
  */
