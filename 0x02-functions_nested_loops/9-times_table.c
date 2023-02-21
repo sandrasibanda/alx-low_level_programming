5452eeb35a4be0616a621de7737a61b9b2cbb220
sandrasibanda <sandra.sibanda992@gmail.com>
2023-02-21 14:51:16 -0800

    timestable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - check description
+ * Description: It prints 9 times table starting with 0
+ * Return: Nothing.
+ */
+void times_table(void)
+{
+	int i, a, n;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (a = 0;a <= 9; a++)
+		{
+			n = i * a;
+
+			if ((n / 10) == 0)
+			{
+				if (a != 0)
+					_putchar(' ');
+				_putchar(n + '0');
+
+				if (a == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
