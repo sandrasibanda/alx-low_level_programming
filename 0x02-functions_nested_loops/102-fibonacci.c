c8905cd96534d20440f7a0b0d58ac161e5d22275
sandrasibanda <sandra.sibanda992@gmail.com>
2023-02-22 01:06:52 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - print first 50 fibonacci
+ * starting with 1 and 2
+ * followed by a new line
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	long int x, y, z, number;
+
+	y = 1;
+
+	z = 2;
+
+	for (x = 1; x <= 50; ++z)
+	{
+		if (y != 20365011074)
+		{
+			printf("%ld, ", y);
+		} else
+		{
+			printf("%ld\n", y);
+		}
+		number = y + z;
+		y = z;
+		z = number;
+	}
+	return (0);
+}
