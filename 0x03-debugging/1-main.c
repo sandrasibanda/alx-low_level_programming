ddc695339c4752e9468887927bb3732dc839b813
sandrasibanda <sandra.sibanda992@gmail.com>
2023-02-23 05:33:23 -0800

    main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,10 +8,10 @@
 int main(void)
 {
 	int i;
+		i=0; 
 
-		printf('Infinite loop incoming : \n');
-	i = 0;
-		printf('Infinite loop avoided! \n');
+		printf("Infinite loop incoming:\n");
+		printf("Infinite loop avoided!\n");
 
 	return (0);
 }
