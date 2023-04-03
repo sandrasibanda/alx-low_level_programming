c6d6fcbcf9fbf56a108c6a3739b91080fece0b6e
sandrasibanda <sandra.sibanda992@gmail.com>
2023-04-03 01:58:48 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*mul - function that multipliestwo numbers
+*@i: first digit
+*@j: second digit
+*Return: returns result
+*/
+
+int mul(int i, int j)
+	{
+		return (i * j);
+	}
+
