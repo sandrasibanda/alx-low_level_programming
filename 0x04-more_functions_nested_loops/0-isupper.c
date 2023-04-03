2a6ba7f36b0285344d5d5501dbe7b1dfc5dca3f0
sandrasibanda <sandra.sibanda992@gmail.com>
2023-04-03 00:32:13 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *isupper - a function that verifies if a char is uppercase or lowercase
+ *@i:test character
+ *Return:returns 1  if char is uppercase else 0
+ *
+ */
+
+int _isupper(int i)
+	{
+	if ((i >= 'A') && (c <= 'Z'))
+	return (1);
+
+	return (0);
+	}	
