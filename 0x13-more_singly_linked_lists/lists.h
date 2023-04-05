be1af5517bcefe037d65394c9daa9ba76ef65e1f
sandrasibanda <sandra.sibanda992@gmail.com>
2023-04-05 01:55:12 -0700

    update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -22,7 +22,7 @@ typedef struct listint_s
 size_t print_listint(const listint_t *a);
 size_t listint_len(const listint_t *a);
 listint_t *add_nodeint(listint_t **head, const int n);
-listint_t *add_nodeint_end(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int a);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
